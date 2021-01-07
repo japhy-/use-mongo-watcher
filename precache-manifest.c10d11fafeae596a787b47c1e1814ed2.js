8efc480f920a4ce3e7ae732b3ae07c9170427f36
Jeffrey Pinyan <jeffrey.pinyan@ithreat.com>
2021-01-07 15:51:14 -0500

    Updates

---
+++ precache-manifest.c10d11fafeae596a787b47c1e1814ed2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d7aab7429aa7fa34f22a4ea1266b1b",
+    "url": "/use-mongo-watcher/index.html"
+  },
+  {
+    "revision": "6b34953c9c2aabea5658",
+    "url": "/use-mongo-watcher/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "6a632932f5358be56730",
+    "url": "/use-mongo-watcher/static/js/2.30f93a02.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/use-mongo-watcher/static/js/2.30f93a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b34953c9c2aabea5658",
+    "url": "/use-mongo-watcher/static/js/main.85b71c01.chunk.js"
+  },
+  {
+    "revision": "13d46641c203d6af86b1",
+    "url": "/use-mongo-watcher/static/js/runtime-main.4044932f.js"
+  }
+]);
