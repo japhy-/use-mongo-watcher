191f028507c0deffbfd6bc81b0dc5820b71d2ca8
Jeffrey Pinyan <jeffrey.pinyan@ithreat.com>
2021-01-08 09:42:21 -0500

    1.1.1 for real

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useMounted } from '@ithreat/use-mounted';
 
 export const useMongoWatcher = ({ collection, filter, callback }) => {
+    const [ config, setConfig ] = useState({collection, filter, callback});
     const stream = useRef();
     const attempt = useRef(0);
     const isMounted = useMounted();
 
     useEffect(() => {
+        const { collection, filter, callback } = config;
+
         // filter must be non-empty
         if (!filter || (Array.isArray(filter) && !filter.length) || !Object.entries(filter).length) return;
 
@@ -14,28 +17,34 @@ export const useMongoWatcher = ({ collection, filter, callback }) => {
         let thisStream;
         const iter = ++attempt.current;
 
-        console.log(`connecting`, { iter, filter });
+        // console.log(`connecting`, { iter, filter });
         collection.watch(filter).then((str) => {
             if (isMounted() && iter === attempt.current) {
-                console.log(`watching`, { iter, filter });
+                // console.log(`watching`, { iter, filter });
                 stream.current = thisStream = str;
                 stream.current.onNext(callback);
             }
             else {
-                console.log(`unmounted while connecting`);
+                // console.log(`unmounted while connecting`);
                 str.close();
             }
         });
 
         return () => {
             if (thisStream) {
-                // console.log(`unwatching`, { iter, filter, thisStream })
+                // console.log(`unwatching`, { iter, filter })
                 thisStream.close();
             }
         }
-    }, [filter, callback]);
+    }, [config]);
 
-    return stream;
+    return {
+        ...config,
+        setCollection: (collection) => setConfig((config) => ({...config, collection})),
+        // only update the filter if it actually LOOKS different
+        setFilter: (filter) => setConfig((config) => JSON.stringify(filter) === JSON.stringify(config.filter) ? config : {...config, filter}),
+        setCallback: (callback) => setConfig((config) => ({...config, callback})),
+    };
 };
 
 export default useMongoWatcher;
